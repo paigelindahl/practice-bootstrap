89544daab9c8670cd37cd00ce87df86635b8aff9
paigelindahl <paigemarlindahl@gmail.com>
2021-01-13 13:55:40 +0000

    practice bootstrap

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.get("/grid", (req, res) => {
   res.render("grid");
 });
 
+app.get("/solo", (req, res) => {
+  res.render("solo");
+});
+
+app.get("/angled", (req, res) => {
+  res.render("angled");
+})
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
